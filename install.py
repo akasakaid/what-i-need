1e7a922dbc9859f127bcbf0eb157e59d644a7677
AkasakaID <fawwazthoerif@gmail.com>
2022-08-10 22:36:36 +0700

    :update add sleep

--- install.py
+++ install.py
@@ -1,8 +1,10 @@
 from os import popen,system
 from time import sleep
+from sys import exit
 
 zenmate = 'https://chrome.google.com/webstore/detail/zenmate-free-vpn%E2%80%93best-vpn/fdcgdnkidjaadafnichfpabhfomcebme?hl=en'
 autorefresh = 'https://chrome.google.com/webstore/detail/auto-refresh-plus-page-mo/hgeljhfekpckiiplhkigfehkdpldcggm?hl=en'
+list_ma_url = ['https://youtu.be/VyGnVrbZXgY','https://youtu.be/5Ucr4pcvWME','https://youtu.be/vUCy7aohKaU','https://youtu.be/pFLGtsoeXaE','https://youtu.be/AJaJWF3IUSA','https://youtu.be/sqol483YaUc']
 
 def startup(name,app):
 	command = 'start ' + name + ' ' + app
@@ -22,9 +24,17 @@ def main():
 		for i in range(start,end+1):
 			nameapp = 'chrome-' + str(i).zfill(3)
 			startup(nameapp,zenmate)
+			sleep(5)
 	elif choice == 2:
 		start = int(input('- start : '))
 		end = int(input('- end : '))
 		for i in range(start,end+1):
 			nameapp = 'chrome-' + str(i).zfill(3)
 			startup(nameapp,autorefresh)
+			sleep(5)
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		exit()
