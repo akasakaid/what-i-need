5e9211e81d8e4cbef63651d51fc1d0c20dcf96a6
AkasakaID <fawwazthoerif@gmail.com>
2022-12-06 20:29:38 +0700

    :update

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-from os import popen,system
+from os import popen, system
 from time import sleep
 from sys import exit
 
@@ -7,75 +7,88 @@ autorefresh = 'https://chrome.google.com/webstore/detail/auto-refresh-plus-page-
 hoxxvpn = 'https://chrome.google.com/webstore/detail/hoxx-vpn-proxy/nbcojefnccbanplpoffopkoepjmhgdgh'
 useragent = 'https://chrome.google.com/webstore/detail/user-agent-switcher-and-m/bhchdcejhohfmigjafbampogmaanbfkg'
 spooftime = 'https://chrome.google.com/webstore/detail/spoof-timezone/kcabmhnajflfolhelachlflngdbfhboe'
+skipads = 'https://chrome.google.com/webstore/detail/youtube-video-skip-ad-tri/hpnelpabemhjfjgiibgkliipaehnfcjk?hl=id'
 
 
-list_ma_url = ['https://youtu.be/VyGnVrbZXgY','https://youtu.be/5Ucr4pcvWME','https://youtu.be/vUCy7aohKaU','https://youtu.be/pFLGtsoeXaE','https://youtu.be/AJaJWF3IUSA','https://youtu.be/sqol483YaUc']
+list_ma_url = ['https://youtu.be/VyGnVrbZXgY', 'https://youtu.be/5Ucr4pcvWME', 'https://youtu.be/vUCy7aohKaU',
+               'https://youtu.be/pFLGtsoeXaE', 'https://youtu.be/AJaJWF3IUSA', 'https://youtu.be/sqol483YaUc']
 
-def startup(name,app=None):
-	if app:
-		command = 'start ' + name + ' ' + app
-		res = popen(command)
-		return
-	else:
-		command = 'start ' + name + ' '
-		res = popen(command)
-		return
+
+def startup(name, app=None):
+    if app:
+        command = 'start ' + name + ' ' + app
+        res = popen(command)
+        return
+    else:
+        command = 'start ' + name + ' '
+        res = popen(command)
+        return
+
 
 def main():
-	system('cls')
-	print('- menu ')
-	print("""
+    system('cls')
+    print('- menu ')
+    print("""
 0. start chrome 
 1. install zenmate vpn
 2. install autorefresh plus
 3. install hoxx vpn
 4. install user agent switcher
 5. install spoofing time
+6. install skipper ads
 		""")
-	print()
-	choice = int(input('-> input number : '))
-	if choice == 0:
-		start = int(input('- start : '))
-		end = int(input('- end : '))
-		for i in range(start,end+1):
-			nameapp = 'chrome-' + str(i).zfill(3)
-			startup(nameapp)
-			sleep(3)
-	if choice == 1:
-		start = int(input('- start : '))
-		end = int(input('- end : '))
-		for i in range(start,end+1):
-			nameapp = 'chrome-' + str(i).zfill(3)
-			startup(nameapp,zenmate)
-			sleep(3)
-	elif choice == 2:
-		start = int(input('- start : '))
-		end = int(input('- end : '))
-		for i in range(start,end+1):
-			nameapp = 'chrome-' + str(i).zfill(3)
-			startup(nameapp,autorefresh)
-			sleep(3)
-	elif choice == 3:
-		start = int(input('- start : '))
-		end = int(input('- end : '))
-		for i in range(start,end+1):
-			nameapp = 'chrome-' + str(i).zfill(3)
-			startup(nameapp,hoxxvpn)
-			sleep(3)
-	elif choice == 4:
-		start = int(input('- start : '))
-		end = int(input('- end : '))
-		for i in range(start,end+1):
-			nameapp = 'chrome-' + str(i).zfill(3)
-			startup(nameapp,useragent)
-			sleep(3)
-	elif choice == 5:
-		start = int(input('- start : '))
-		end = int(input('- end : '))
-		for i in range(start,end+1):
-			nameapp = 'chrome-' + str(i).zfill(3)
-			startup(nameapp,spooftime)
-			sleep(3)
+    print()
+    choice = int(input('-> input number : '))
+    if choice == 0:
+        start = int(input('- start : '))
+        end = int(input('- end : '))
+        for i in range(start, end+1):
+            nameapp = 'chrome-' + str(i).zfill(3)
+            startup(nameapp)
+            sleep(3)
+    if choice == 1:
+        start = int(input('- start : '))
+        end = int(input('- end : '))
+        for i in range(start, end+1):
+            nameapp = 'chrome-' + str(i).zfill(3)
+            startup(nameapp, zenmate)
+            sleep(3)
+    elif choice == 2:
+        start = int(input('- start : '))
+        end = int(input('- end : '))
+        for i in range(start, end+1):
+            nameapp = 'chrome-' + str(i).zfill(3)
+            startup(nameapp, autorefresh)
+            sleep(3)
+    elif choice == 3:
+        start = int(input('- start : '))
+        end = int(input('- end : '))
+        for i in range(start, end+1):
+            nameapp = 'chrome-' + str(i).zfill(3)
+            startup(nameapp, hoxxvpn)
+            sleep(3)
+    elif choice == 4:
+        start = int(input('- start : '))
+        end = int(input('- end : '))
+        for i in range(start, end+1):
+            nameapp = 'chrome-' + str(i).zfill(3)
+            startup(nameapp, useragent)
+            sleep(3)
+    elif choice == 5:
+        start = int(input('- start : '))
+        end = int(input('- end : '))
+        for i in range(start, end+1):
+            nameapp = 'chrome-' + str(i).zfill(3)
+            startup(nameapp, spooftime)
+            sleep(3)
+    elif choice == 6:
+        start = int(input('- start : '))
+        end = int(input('- end : '))
+        for i in range(start, end+1):
+            nameapp = 'chrome-' + str(i).zfill(3)
+            startup(nameapp, skipads)
+            sleep(3)
+
 
 if __name__ == "__main__":
     try:
